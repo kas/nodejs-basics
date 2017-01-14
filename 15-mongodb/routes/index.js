2c6f8e08f161fd92b2e8a1c44c69497e908a5436
Kenneth Schnall <kenneth.schnall@gmail.com>
2017-01-13 22:03:00 -0500

    Finish video 15

--- 15-mongodb/routes/index.js
+++ 15-mongodb/routes/index.js
@@ -1,18 +1,55 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb').MongoClient;
+var assert = require('assert');
+
+var url = '';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Custom title', condition: true, anyArray: [1,2,3] });
+  res.render('index');
 });
 
-router.get('/test/:id', function(req, res, next) {
- res.render('test', {id: req.params.id});
+router.get('/get-data', function(req, res, next) {
+  var resultArray = [];
+  mongo.connect(url, function(err, db) { // this block runs asynchronously
+    assert.equal(null, err);
+    var cursor = db.collection('user-data').find(); // get all entries
+    cursor.forEach(function(doc, err) {
+      assert.equal(null, err);
+      resultArray.push(doc);
+    }, function() { // called after all iterations are done
+      db.close();
+      res.render('index', { items: resultArray });
+    });
+  });
 });
 
-router.post('/test/submit', function(req, res, next) {
-  var id = req.body.id;
-  res.redirect('/test/' + id);
+router.post('/insert', function(req, res, next) {
+  var item = {
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  };
+
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('user-data').insertOne(item, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item inserted');
+      db.close();
+    });
+  });
+
+  res.redirect('/');
+});
+
+router.post('/update', function(req, res, next) {
+
+});
+
+router.post('/delete', function(req, res, next) {
+
 });
 
 module.exports = router;
